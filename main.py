237859c0422fe2bc0036f8cc01ef33049534162a
ilton <xlsonplay@gmail.com>
2022-06-23 20:00:26 -0300

    Função criar criada com sucesso com seguintes registros: Nome, Idade, Momento escolar

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import shutil
+from time import sleep
 
-def apresentacao():
-    print("")
+
+#Função que vai criar o registro do aluno
+def criarAluno(nome, idade, epoca):
+    diretorio = 'F:/Projetos Python/cadastrar_alunos_crud/Alunos.txt'
+    arquivo = open(diretorio,'a')
+    arquivo.writelines('\nNome: '+nome+'\nIdade: '+idade+'\nTurma Escolar: '+epoca+'\n')
+    arquivo.close()
+
 
 
 
 
 if __name__ == '__main__':
-    apresentacao()
+    opcao = int(input('Oque deseja fazer?\n01 - Cadastrar Aluno\n02 - Mostrar Alunos cadastrado\n03 - Atualizar Registro'
+                      '04 - Excluir registro\nEscolha de acordo com o numero sem o zero: '))
+    if opcao == 1:
+        qt_aluno = int(input('Insira a quantidade de alunos que vão ser cadastrada:'))
+        contador = 0
+        while contador < qt_aluno:
+            contador+=1
+            aluno = str(input('Insira o nome dos alunos: '))
+            idade = str(input('Insira a idade do aluno: '))
+            epoca_escolar = str(input('Insira o ano escolar do aluno: '))
+            criarAluno(aluno,idade,epoca_escolar)
+            print('Adicionando, um segundo...')
+            sleep(1)
+        print('Tudo adicionado com sucesso')
 
