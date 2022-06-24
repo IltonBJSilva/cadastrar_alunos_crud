c9fa536e9f0d91741674fab6737d42dcad5a24a4
ilton <xlsonplay@gmail.com>
2022-06-24 17:34:23 -0300

    Função para visualizar as informações criada com sucesso

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def mostar_alunos(nome_arquivo):
 
 
 if __name__ == '__main__':
-    arquivo = 'F:/Projetos Python/cadastrar_alunos_crud/Alunos.txt'    lista_alunos = {'aluno':'','idade':'','turma':''}
+    arquivo = 'F:/Projetos Python/cadastrar_alunos_crud/Alunos.txt'
+    lista_aluno = []
     while True:
         opcao = int(input('Oque deseja fazer?\n00 - Fechar \n01 - Cadastrar Aluno\n02 - Mostrar Alunos cadastrado\n03 - Atualizar Registro'
                   '\n04 - Excluir registro\nEscolha de acordo com o numero sem o zero: '))
@@ -32,19 +33,27 @@ if __name__ == '__main__':
             contador = 0
             while contador < qt_aluno:
                 contador+=1
+                dic_alunos = ({'alunoid': [], 'aluno': [], 'idade': [], 'turma': []})
                 aluno = str(input('Insira o nome dos alunos: '))
                 idade = str(input('Insira a idade do aluno: '))
                 epoca_escolar = str(input('Insira o ano escolar do aluno: '))
-                lista_alunos['aluno'] = aluno
-                lista_alunos['idade'] = idade
-                lista_alunos['turma'] = epoca_escolar
+                dic_alunos['alunoid'] = contador
+                dic_alunos['aluno'] = aluno
+                dic_alunos['idade'] = idade
+                dic_alunos['turma'] = epoca_escolar
                 criarAluno(aluno, idade, epoca_escolar)
+                lista_aluno.append(dic_alunos)
+
+                #lista_aluno.append(dic_alunos['alunoid'])
+                # lista_aluno.append(dic_alunos['aluno'])
+                # lista_aluno.append(dic_alunos['idade'])
+                # lista_aluno.append(dic_alunos['turma'])
+
 
                 print('Adicionando, um segundo...')
                 sleep(1)
             print('Tudo adicionado com sucesso')
-            print(lista_alunos)
-
+            print(lista_aluno)
         if opcao == 2: #Mostrar Arquivo
             if os.path.isfile(arquivo):
                 print(f'O caminho {arquivo} existe, estamos abrindo so um segundo...')
@@ -52,6 +61,15 @@ if __name__ == '__main__':
                 mostar_alunos('Alunos.txt')
             else:
                 print('Não existe nenhum aluno cadastrado, deseja criar um registro?')
+        if opcao == 3: #Atualizar informação
+            if os.path.isfile(arquivo):
+                indice = str(input(f'Qual deseja excluir?\n{lista_aluno}'))
+                sleep(1)
+                mostar_alunos('Alunos.txt')
+            else:
+                print('Não existe nenhum aluno cadastrado, deseja criar um registro?')
+
+
 
         else:
             print('Não existe essa opcao')
